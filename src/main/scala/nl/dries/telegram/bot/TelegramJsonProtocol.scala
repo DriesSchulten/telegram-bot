5be92ee777c56032f966dbd5ed3aa452f4b5f049
Dries Schulten <dries.schulten@topicus.nl>
2015-09-29 15:13:43 +0200

    Changed date to a real date with custom serializer.

--- src/main/scala/nl/dries/telegram/bot/TelegramJsonProtocol.scala
+++ src/main/scala/nl/dries/telegram/bot/TelegramJsonProtocol.scala
@@ -1,10 +1,12 @@
 package nl.dries.telegram.bot
 
+import java.util.Date
+
 import spray.json._
 
 sealed trait MessageSender
 
-case class Message(id: Int, date: Int, text: Option[String], chat: Either[User, GroupChat], from: User)
+case class Message(id: Int, date: Date, text: Option[String], chat: Either[User, GroupChat], from: User)
 
 case class Update(id: Int, message: Message)
 
@@ -15,6 +17,15 @@ case class User(id: Int, firstName: String, lastName: String, username: String) 
 case class GroupChat(id: Int, title: String) extends MessageSender
 
 object TelegramJsonProtocol extends DefaultJsonProtocol {
+  implicit object DateJsonFormat extends JsonFormat[Date] {
+    override def write(obj: Date): JsValue = JsNumber(obj.getTime / 1000)
+
+    override def read(json: JsValue): Date = json match {
+      case JsNumber(value) => new Date(value.toLong * 1000)
+      case _ => throw new DeserializationException("Expected value not in epoch")
+    }
+  }
+
   implicit val groupChatFormat = jsonFormat2(GroupChat)
   implicit val userFormat = jsonFormat(User, "id", "first_name", "last_name", "username")
   implicit val messageFormat = jsonFormat(Message, "message_id", "date", "text", "chat", "from")
--- src/test/scala/nl/dries/telegram/bot/TelegramJsonProtocolSpec.scala
+++ src/test/scala/nl/dries/telegram/bot/TelegramJsonProtocolSpec.scala
@@ -1,5 +1,7 @@
 package nl.dries.telegram.bot
 
+import java.util.Date
+
 import nl.dries.telegram.bot.TelegramJsonProtocol._
 import org.scalatest._
 import spray.json._
@@ -9,9 +11,11 @@ import spray.json._
  */
 class TelegramJsonProtocolSpec extends FunSuite with ShouldMatchers {
 
+  val timeInSeconds = new Date().getTime / 1000
+
   test("A message should be able to serialize to/from JSON with a user") {
     val user = User(2, "Dries", "Schulten", "Dries_Schulten")
-    val message = Message(1, 12356, Some("text"), Left(user), user)
+    val message = Message(1, new Date(timeInSeconds * 1000), Some("text"), Left(user), user)
 
     val json = message.toJson
     assert(json.convertTo[Message] === message)
@@ -20,7 +24,7 @@ class TelegramJsonProtocolSpec extends FunSuite with ShouldMatchers {
   test("A message should be able to serialize to/from JSON with a group chat") {
     val user = User(2, "Dries", "Schulten", "Dries_Schulten")
     val group = GroupChat(3, "Group chat")
-    val message = Message(1, 12305, Some("message"), Right(group), user)
+    val message = Message(1, new Date(timeInSeconds * 1000), Some("message"), Right(group), user)
 
     val json = message.toJson
     assert(json.convertTo[Message] === message)
