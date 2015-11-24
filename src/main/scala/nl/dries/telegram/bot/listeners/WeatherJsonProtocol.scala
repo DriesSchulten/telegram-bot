8e4cdc07e5c80d32fb08260a88d510c86d85fd81
Dries Schulten <dries.schulten@gmail.com>
2015-11-24 14:59:42 +0100

    Extra 'Weather' from JSON.

--- src/main/scala/nl/dries/telegram/bot/listeners/WeatherJsonProtocol.scala
+++ src/main/scala/nl/dries/telegram/bot/listeners/WeatherJsonProtocol.scala
@@ -1,4 +1,37 @@
 package nl.dries.telegram.bot.listeners
 
+import org.json4s.DefaultFormats
+import org.json4s.native.JsonParser
+import spray.http.{HttpCharsets, HttpEntity, MediaTypes}
+import spray.httpx.unmarshalling.Unmarshaller
+
 case class Weather(description: String, temperature: Double, rain: Double, windSpeed: Double, direction: Int)
 
+trait WeatherJsonProtocol {
+
+  implicit val formats = DefaultFormats
+
+  /**
+    * Parse weather out of a JSON string
+    * @param content given content in JSON
+    * @return Weather object constructed out of JSON data
+    */
+  def parseWeather(content: String): Weather = {
+    val json = JsonParser.parse(content)
+    val description = (json \ "weather") (0) \ "description"
+    val temperature = json \ "main" \ "temp"
+    val rain = json \ "rain" \ "3h"
+    val windSpeed = json \ "wind" \ "speed"
+    val windDirection = json \ "wind" \ "deg"
+
+    Weather(description.extract[String], temperature.extract[Double], rain.extract[Double], windSpeed.extract[Double], windDirection.extract[Int])
+  }
+
+  /**
+    * Spray unmarshaller to extract 'Weather' from a JSON HTTP response
+    */
+  implicit val weatherUnmarshaller = Unmarshaller[Weather](MediaTypes.`application/json`) {
+    case x: HttpEntity.NonEmpty => parseWeather(x.asString(HttpCharsets.`UTF-8`))
+    case HttpEntity.Empty => throw new IllegalStateException("Not expecting empty result")
+  }
+}
--- src/test/scala/nl/dries/telegram/bot/listeners/WeatherJsonProtocolSpec.scala
+++ src/test/scala/nl/dries/telegram/bot/listeners/WeatherJsonProtocolSpec.scala
@@ -5,14 +5,23 @@ import org.scalatest.{FunSuite, Matchers}
 /**
   * Specs for the Weather JSON protocol
   */
-class WeatherJsonProtocolSpec extends FunSuite with Matchers {
+class WeatherJsonProtocolSpec extends FunSuite with Matchers with WeatherJsonProtocol {
 
-  test("A message should be deserialized") {
+  test("A weather object should be deserialized from a JSON string") {
     val fileContent: Option[String] = for {
       resource <- Option(getClass.getResourceAsStream("/json/weather.json"))
       bufferedResource <- Option(io.Source.fromInputStream(resource))
       content <- Option(bufferedResource.mkString)
     } yield content
+
+    fileContent match {
+      case Some(json) => {
+        val weather = parseWeather(json)
+        weather.description shouldBe "broken clouds"
+        weather.temperature shouldBe 293.25
+      }
+      case None => fail("Could not read test data")
+    }
   }
 
 }
